15b787edb14fd769c724d9d14cca6282293fcfb8
Karol <karolp2003@gmail.com>
2024-07-30 12:42:59 +0200

    .

--- modules/bar/notifications/index.ts
+++ modules/bar/notifications/index.ts
@@ -1,9 +1,3 @@
-import Gdk from "gi://Gdk?version=3.0";
-import options from "options";
-
-const { show_total } = options.bar.notifications;
-const { terminal } = options;
-
 const notifs = await Service.import("notifications");
 
 export const Notifications = () => {
@@ -14,27 +8,20 @@ export const Notifications = () => {
         hpack: "start",
         class_name: "bar-notifications",
         children: Utils.merge(
-          [
-            notifs.bind("notifications"),
-            notifs.bind("dnd"),
-            show_total.bind("value"),
-          ],
-          (notif, dnd, showTotal) => {
+          [notifs.bind("notifications"), notifs.bind("dnd")],
+          (notif, dnd) => {
             const notifIcon = Widget.Label({
               hpack: "center",
-              class_name: "bar-notifications-label",
+              class_name: "bar-button-icon notifications",
               label: dnd ? "󰂛" : notif.length > 0 ? "󱅫" : "󰂚",
             });
 
             const notifLabel = Widget.Label({
               hpack: "center",
-              class_name: "bar-notifications-total",
+              class_name: "bar-button-label notifications",
               label: notif.length.toString(),
             });
 
-            if (showTotal) {
-              return [notifIcon, notifLabel];
-            }
             return [notifIcon];
           }
         ),
@@ -43,7 +30,7 @@ export const Notifications = () => {
     isVisible: true,
     boxClass: "notifications",
     props: {
-      on_primary_click: (clicked: any, event: Gdk.Event) => {
+      on_primary_click: () => {
         Utils.execAsync("swaync-client -t").catch((err) => console.error(err));
       },
     },
