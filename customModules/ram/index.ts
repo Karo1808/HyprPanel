5915e764a40d4e5d3c95bcae5c3194fda373a252
Karol <karolp2003@gmail.com>
2024-09-06 12:42:35 +0200

    merge

--- customModules/ram/index.ts
+++ customModules/ram/index.ts
@@ -1,7 +1,7 @@
 import options from "options";
 
 // Module initializer
-import { module } from "../module"
+import { module } from "../module";
 
 // Types
 import { GenericResourceData } from "lib/types/customModules/generic";
@@ -12,7 +12,11 @@ import Gtk from "types/@girs/gtk-3.0/gtk-3.0";
 import { calculateRamUsage } from "./computeRam";
 
 // Utility Methods
-import { formatTooltip, inputHandler, renderResourceLabel } from "customModules/utils";
+import {
+  formatTooltip,
+  inputHandler,
+  renderResourceLabel,
+} from "customModules/utils";
 import { ResourceLabelType } from "lib/types/bar";
 
 // Global Constants
@@ -21,68 +25,84 @@ import { pollVariable } from "customModules/PollVar";
 
 // All the user configurable options for the ram module that are needed
 const {
-    label,
-    labelType,
-    round,
-    leftClick,
-    rightClick,
-    middleClick,
-    pollingInterval
+  label,
+  labelType,
+  round,
+  leftClick,
+  rightClick,
+  middleClick,
+  pollingInterval,
 } = options.bar.customModules.ram;
 
-const defaultRamData: GenericResourceData = { total: 0, used: 0, percentage: 0, free: 0 };
+const defaultRamData: GenericResourceData = {
+  total: 0,
+  used: 0,
+  percentage: 0,
+  free: 0,
+};
 const ramUsage = Variable(defaultRamData);
 
 pollVariable(
-    ramUsage,
-    [round.bind('value')],
-    pollingInterval.bind('value'),
-    calculateRamUsage,
-    round,
+  ramUsage,
+  [round.bind("value")],
+  pollingInterval.bind("value"),
+  calculateRamUsage,
+  round
 );
 
 export const Ram = () => {
+  const ramModule = module({
+    textIcon: "",
+    label: Utils.merge(
+      [ramUsage.bind("value"), labelType.bind("value"), round.bind("value")],
+      (
+        rmUsg: GenericResourceData,
+        lblTyp: ResourceLabelType,
+        round: boolean
+      ) => {
+        const returnValue = renderResourceLabel(lblTyp, rmUsg, round);
 
-    const ramModule = module({
-        textIcon: "",
-        label: Utils.merge(
-            [ramUsage.bind("value"), labelType.bind("value"), round.bind("value")],
-            (rmUsg: GenericResourceData, lblTyp: ResourceLabelType, round: boolean) => {
-                const returnValue = renderResourceLabel(lblTyp, rmUsg, round);
-
-                return returnValue;
-            }),
-        tooltipText: labelType.bind("value").as(lblTyp => {
-            return formatTooltip('RAM', lblTyp);
-        }),
-        boxClass: "ram",
-        showLabelBinding: label.bind("value"),
-        props: {
-            setup: (self: Button<Gtk.Widget, Gtk.Widget>) => {
-                inputHandler(self, {
-                    onPrimaryClick: {
-                        cmd: leftClick,
-                    },
-                    onSecondaryClick: {
-                        cmd: rightClick,
-                    },
-                    onMiddleClick: {
-                        cmd: middleClick,
-                    },
-                    onScrollUp: {
-                        fn: () => {
-                            labelType.value = LABEL_TYPES[(LABEL_TYPES.indexOf(labelType.value) + 1) % LABEL_TYPES.length] as ResourceLabelType;
-                        }
-                    },
-                    onScrollDown: {
-                        fn: () => {
-                            labelType.value = LABEL_TYPES[(LABEL_TYPES.indexOf(labelType.value) - 1 + LABEL_TYPES.length) % LABEL_TYPES.length] as ResourceLabelType;
-                        }
-                    },
-                });
+        return returnValue;
+      }
+    ),
+    tooltipText: labelType.bind("value").as((lblTyp) => {
+      return formatTooltip("RAM", lblTyp);
+    }),
+    boxClass: "ram",
+    showLabelBinding: label.bind("value"),
+    props: {
+      setup: (self: Button<Gtk.Widget, Gtk.Widget>) => {
+        inputHandler(self, {
+          onPrimaryClick: {
+            cmd: leftClick,
+          },
+          onSecondaryClick: {
+            cmd: rightClick,
+          },
+          onMiddleClick: {
+            cmd: middleClick,
+          },
+          onScrollUp: {
+            fn: () => {
+              labelType.value = LABEL_TYPES[
+                (LABEL_TYPES.indexOf(labelType.value) + 1) % LABEL_TYPES.length
+              ] as ResourceLabelType;
             },
-        }
-    });
+          },
+          onScrollDown: {
+            fn: () => {
+              labelType.value = LABEL_TYPES[
+                (LABEL_TYPES.indexOf(labelType.value) -
+                  1 +
+                  LABEL_TYPES.length) %
+                  LABEL_TYPES.length
+              ] as ResourceLabelType;
+            },
+          },
+        });
+      },
+    },
+  });
 
-    return ramModule;
-}
+  return ramModule;
+};
